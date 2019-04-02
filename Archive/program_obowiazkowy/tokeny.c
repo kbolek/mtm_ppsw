e600828bd8da2333e2fb6623cbc81d2b333e59fc
kbolek <kbolekagh@gmail.com>
2019-04-03 00:30:38 +0200

    Dodanie tokenów do obowiązkowych programów

---
+++ Archive/program_obowiazkowy/tokeny.c
@@ -0,0 +1,178 @@
+#define NULL 0
+#define MAX_TOKEN_NR 4
+#define MAX_KEYWORD_STRING_LTH 20
+#define MAX_KEYWORD_NR 3
+
+enum KeywordCode {MTM, KB, PPSW};
+enum CompResult {DIFFERENT , EQUAL };
+enum Stan {TOKEN,DELIMITER};
+enum Result {OK, ERROR};
+unsigned char ucTokenNr = 0;
+
+union TokenValue {
+	enum KeywordCode eKeyword; 
+	unsigned int uiNumber;
+	char *pcString;
+};
+
+enum TokenType {KEYWORD, NUMBER, STRING};
+struct Token{
+	enum TokenType eType;
+	union TokenValue uValue;
+};
+struct Token asToken[MAX_TOKEN_NR];
+
+
+struct Keyword{
+	enum KeywordCode eCode;
+	char cString[MAX_KEYWORD_STRING_LTH+1];
+};
+
+struct Keyword asKeywordList[MAX_KEYWORD_NR] = {
+	{MTM,"mikroelektronika"},
+	{KB, "Krzysztof"},
+	{PPSW, "programowanie"}
+};
+
+
+enum CompResult eCompareString(char pcStr1[], char pcStr2[])
+{
+	unsigned char ucCharCounter;
+	for(ucCharCounter=0;pcStr1[ucCharCounter] != '\0';ucCharCounter++)
+	{
+		if (pcStr1[ucCharCounter] != pcStr2[ucCharCounter]) return DIFFERENT;
+	}
+	return EQUAL;
+}
+
+
+enum Result eHexStringToUInt(char pcStr[],unsigned int *puiValue){
+	unsigned char ucHexCounter, ucHexCurrentNumber; 
+	
+	if(pcStr[0] != '0' || pcStr[1] != 'x' || pcStr[2] == NULL){
+		return ERROR;
+	}
+	*puiValue = 0;
+	for(ucHexCounter = 2; pcStr[ucHexCounter] != NULL; ucHexCounter++){
+		ucHexCurrentNumber = pcStr[ucHexCounter];
+		
+		if(ucHexCurrentNumber == 6){
+			return ERROR;
+		}
+		*puiValue = *puiValue << 4;
+		if((ucHexCurrentNumber >= '0') && (ucHexCurrentNumber <= '9') ){	
+			 *puiValue = *puiValue | (ucHexCurrentNumber - '0');
+		}
+		else if((ucHexCurrentNumber >= 'A') && (ucHexCurrentNumber <= 'F')){
+			  *puiValue = *puiValue	| (ucHexCurrentNumber - 'A' + 10);
+		}
+	}
+	return OK;
+}
+ 
+void ReplaceCharactersInString(char pcString[], char cOldChar,char cNewChar) {
+	unsigned char ucLoopCounter;
+	for(ucLoopCounter = 0; pcString[ucLoopCounter]; ucLoopCounter++){
+		if(pcString[ucLoopCounter] == cOldChar){
+			pcString[ucLoopCounter] = cNewChar;
+		}
+	}
+}
+
+
+unsigned char ucFindTokensInString (char *pcString){
+	unsigned char ucCharacterCounter;
+	unsigned char ucCurrentCharacter;
+	enum Stan eStan = DELIMITER;
+	
+	for(ucCharacterCounter=0;;ucCharacterCounter++){
+		ucCurrentCharacter = pcString[ucCharacterCounter];
+		switch(eStan){
+			case TOKEN:
+			{
+				if(ucTokenNr==MAX_TOKEN_NR){
+					return ucTokenNr;
+				}
+				else if(ucCurrentCharacter==NULL){
+					return ucTokenNr;
+				}
+				else if(ucCurrentCharacter!=' '){
+					eStan=TOKEN;
+				}
+				else{
+					eStan=DELIMITER;
+				}
+				break;
+			}
+			case DELIMITER:
+			{
+				if(ucCurrentCharacter==NULL){
+					return ucTokenNr;
+				}
+				else if(ucCurrentCharacter==' '){
+					eStan=DELIMITER;
+				}
+				else{
+					eStan=TOKEN;
+					asToken[ucTokenNr].uValue.pcString = pcString + ucCharacterCounter;
+					ucTokenNr = ucTokenNr + 1;
+				}
+				break;
+			}
+		}
+	}
+};
+
+
+enum Result eStringToKeyword(char pcStr[],enum KeywordCode *peKeywordCode){
+	unsigned char ucTokenCounter;
+	for(ucTokenCounter=0;ucTokenCounter<MAX_TOKEN_NR;ucTokenCounter++)
+	{
+		if (eCompareString(pcStr,asKeywordList[ucTokenCounter].cString) == EQUAL) 
+		{
+			*peKeywordCode = asKeywordList[ucTokenCounter].eCode;
+			return OK;
+		}
+	}
+	return ERROR;
+};
+
+
+
+void DecodeTokens() {
+	unsigned char ucTokenCounter;
+	struct Token *psCurrentToken;
+	unsigned int uiTokenValue;
+	enum KeywordCode eTokenCode;
+
+	for(ucTokenCounter= 0; ucTokenCounter< ucTokenNr; ucTokenCounter++){
+		psCurrentToken= &asToken[ucTokenCounter];
+
+		if(OK == eHexStringToUInt(psCurrentToken->uValue.pcString, &uiTokenValue)){
+			psCurrentToken->eType = NUMBER;
+			psCurrentToken->uValue.uiNumber = uiTokenValue;
+		}
+		else if(OK == eStringToKeyword(psCurrentToken->uValue.pcString, &eTokenCode)){
+			psCurrentToken->eType=KEYWORD;
+			psCurrentToken->uValue.eKeyword=eTokenCode;
+		}
+		else{
+			psCurrentToken->eType=STRING;
+		}
+	}
+	}
+
+void DecodeMsg(char *pcString){
+
+	ucTokenNr = ucFindTokensInString(pcString);
+	ReplaceCharactersInString(pcString, ' ', NULL);
+	DecodeTokens();
+}
+
+
+
+int main(){
+	char pcString[35] = "Krzysztof Bolek mikroelektronika";
+	DecodeMsg(pcString);
+}
+
